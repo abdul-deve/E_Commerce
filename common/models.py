d021d2016e297e6335be13678301e39d34a6d3ff
Abdul Mateen <abdulmateenaawan@gmail.com>
2025-07-20 21:49:25 +0500

    set user model

--- common/models.py
+++ common/models.py
@@ -1,5 +1,22 @@
 from django.db import models
+import  uuid
+def abstract_model(cls):
+    class Meta:
+        abstract = True
 
+    cls.Meta = type(
+        'Meta',
+        (getattr(cls, 'Meta', object),),
+        {'abstract': True}
+    )
+    return cls
+
+@abstract_model
 class TimeStamp(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+@abstract_model
+class UniqueID(models.Model):
+    id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4())
+
--- user/models.py
+++ user/models.py
@@ -1,23 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from common.models import TimeStamp
-import uuid
+from common.models import TimeStamp,UniqueID
 
 
 
 
 
-class User(AbstractUser,TimeStamp):
-    id = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4())
+class User(AbstractUser,TimeStamp,UniqueID):
     email = models.EmailField(max_length=250,unique=True,db_index=True)
     user_name = models.CharField(max_length=250,unique=True,db_index=True)
 
 
-class Profile(TimeStamp):
+class Profile(TimeStamp,UniqueID):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     name  = models.CharField(max_length=250)
     profile_pic = models.ImageField(null=True,blank=True)
     phone_number = models.PositiveSmallIntegerField(max_length=20,null=True,blank=True,unique=True)
+
+class Address(TimeStamp,UniqueID):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+
+
 
 
 
