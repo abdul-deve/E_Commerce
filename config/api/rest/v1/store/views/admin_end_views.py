a6090aa27db27bb57894ca8869d780ab92cdc80d
Abdul Mateen <abdulmateenaawan@gmail.com>
2025-08-08 21:19:49 +0500

    sets admin_end views

---
+++ config/api/rest/v1/store/views/admin_end_views.py
@@ -0,0 +1,30 @@
+
+from store.models import (Product,
+                            Category,
+                            Inventory,)
+
+from config.api.rest.v1.store.serializers.admin_end__serializers import (CategorySerializer,
+                                                                         ProductSerializer,
+                                                                         InventorySerializer,
+                                                                         ProductImageSerializer)
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import  status
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset =  Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes =  []
+    authentication_classes =  []
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = []
+    authentication_classes = []
+
+
